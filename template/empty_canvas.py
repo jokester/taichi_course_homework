75f264ed15247191d436166c7ab0b548000a1ace
Wang Guan <momocraft@gmail.com>
2021-10-26 16:16:54 +0000

    create canvas

--- template/empty_canvas.py
+++ template/empty_canvas.py
@@ -1,27 +1,24 @@
 # a main program with empty canvas
-
 import taichi as ti
 
 ti.init(arch=ti.gpu)
 
-canvas_w = 512
-canvas_h = 512
-
-pixels = ti.field(dtype=float, shape=(canvas_w, canvas_h))
-
-gui = ti.GUI("Julia Set", res=(canvas_w, canvas_h))
-gui.set_image(pixels)
-gui.show()
+canvas_w = 960
+canvas_h = 540
 
 @ti.kernel
-def draw_frame(frame_no: ti.i32):
-    for x, y in pixels:
-        pass
+def draw_frame(buffer: ti.template(), frame_no: ti.i32):
+    for x, y in buffer:
+        buffer[x, y] = ti.Vector([1.0 * x / canvas_w, 1.0 * y / canvas_h, 0.0])
 
+pixels = ti.Vector.field(3, dtype=ti.f32, shape=(canvas_w, canvas_h))
+gui = ti.GUI("Empty Canvas", res=(canvas_w, canvas_h))
 
 for frame_no in range(0, 1000000000):
-    draw_frame(frame_no)
-    if frame_no % 1000 == 0:
-        print("drawing frame #%i" % (frame_no))
-    if not gui.running:
+    draw_frame(pixels, frame_no)
+    gui.set_image(pixels)
+    if gui.running:
+        gui.show()
+    else:
+        print("finished at frame #%d" % (frame_no) )
         exit(0)
