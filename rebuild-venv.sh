4c735f027cf7959c75b4257fd328db6474a079ed
Wang Guan <momocraft@gmail.com>
2021-11-22 23:25:57 +0900

    upgrade taichi

--- rebuild-venv.sh
+++ rebuild-venv.sh
@@ -6,7 +6,7 @@ set -uex
 if [[ ! -e venv ]]; then
   python3 -m venv venv
   venv/bin/pip3 install wheel pipdeptree autopep8 # dev deps
-  venv/bin/pip3 install "taichi<0.8.0"
+  venv/bin/pip3 install "taichi<0.9"
   venv/bin/pip3 freeze > requirements.txt
 else
   echo "'venv' directory already exists. remove it and run $0 again to rebuild all deps"
