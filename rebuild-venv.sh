082fa020a4c1b5fc8280e527158cea14cee4bc83
Wang Guan <momocraft@gmail.com>
2021-12-15 15:09:09 +0000

    update

--- rebuild-venv.sh
+++ rebuild-venv.sh
@@ -3,10 +3,13 @@
 cd $(dirname "$0")
 set -uex
 
+__OUR_PYTHON=python3
+#__OUR_PYTHON=/opt/miniconda/bin/python3.9
+
 if [[ ! -e venv ]]; then
-  python3 -m venv venv
+  command $__OUR_PYTHON -m venv venv
   venv/bin/pip3 install wheel pipdeptree autopep8 # dev deps
-  venv/bin/pip3 install "taichi<0.9"
+  venv/bin/pip3 install "taichi"
   venv/bin/pip3 freeze > requirements.txt
 else
   echo "'venv' directory already exists. remove it and run $0 again to rebuild all deps"
